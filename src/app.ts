0369f1ee278f10e6c1b7e1fadbbbaac123625121
krembler <marjoribanks.d@gmail.com>
2022-06-02 14:13:46 +0100

    refactored routing methods

--- src/app.ts
+++ src/app.ts
@@ -5,23 +5,42 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended:true }));
 
-app.get('/', (req: Request, res: Response) =>{
-  return res.json({
-    success: true,
-    name: "DuncansExpressApp"
-  });
+app
+.route('/')
+.get( (req: Request, res: Response) =>{
+  return res.send("You made a GET request");
+})
+.post( (req: Request, res: Response) =>{
+  return res.send("You made a POST request");
+})
+.put( (req: Request, res: Response) =>{
+  return res.send("You made a PUT request");
+})
+.delete( (req: Request, res: Response) =>{
+  return res.send("You made a DELETE request");
+})
+.all( (req: Request, res: Response) =>{
+  return res.send("You made an X request");
 });
 
-app.post('/api/data', (req: Request, res: Response) =>{
-  console.log(req.body);
 
-  return res.sendStatus(200);
-});
-
-app.all('/api/all', (req: Request, res: Response) =>{
-
-  return res.sendStatus(200);
-});
+// app.get('/', (req: Request, res: Response) =>{
+//   return res.json({
+//     success: true,
+//     name: "DuncansExpressApp"
+//   });
+// });
+//
+// app.post('/api/data', (req: Request, res: Response) =>{
+//   console.log(req.body);
+//
+//   return res.sendStatus(200);
+// });
+//
+// app.all('/api/all', (req: Request, res: Response) =>{
+//
+//   return res.sendStatus(200);
+// });
 
 app.listen(3000, ()=>{
   console.log("appliaction listening at http://localhost:3000")
