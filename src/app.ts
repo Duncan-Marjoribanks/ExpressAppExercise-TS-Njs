3cb2882fa83510f8f7ade812aa0c0be8f54fdfcc
krembler <marjoribanks.d@gmail.com>
2022-06-02 14:21:05 +0100

    routes to patterns

--- src/app.ts
+++ src/app.ts
@@ -5,25 +5,33 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended:true }));
 
-app
-.route('/')
-.get( (req: Request, res: Response) =>{
-  return res.send("You made a GET request");
-})
-.post( (req: Request, res: Response) =>{
-  return res.send("You made a POST request");
-})
-.put( (req: Request, res: Response) =>{
-  return res.send("You made a PUT request");
-})
-.delete( (req: Request, res: Response) =>{
-  return res.send("You made a DELETE request");
-})
-.all( (req: Request, res: Response) =>{
-  return res.send("You made an X request");
-});
 
+//3rd pass routes (string patterns)
+app.get('/health', (req: Request, res: Response) => res.sendStatus(200))
+app.get('/abc', (req: Request, res: Response) => res.send("abc"))
+app.get('/ab*cd', (req: Request, res: Response) => res.send("ab*cd"))
+app.get(/xyz/, (req: Request, res: Response) => res.send("xyz"))
 
+//2nd pass routes
+// app
+// .route('/')
+// .get( (req: Request, res: Response) =>{
+//   return res.send("You made a GET request");
+// })
+// .post( (req: Request, res: Response) =>{
+//   return res.send("You made a POST request");
+// })
+// .put( (req: Request, res: Response) =>{
+//   return res.send("You made a PUT request");
+// })
+// .delete( (req: Request, res: Response) =>{
+//   return res.send("You made a DELETE request");
+// })
+// .all( (req: Request, res: Response) =>{
+//   return res.send("You made an X request");
+// });
+
+//1st pass routes
 // app.get('/', (req: Request, res: Response) =>{
 //   return res.json({
 //     success: true,
